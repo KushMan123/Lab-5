3dc134968af8fa01fbeaffdc3d2ee513c3451995
Kushal Manandhar <manandharkushal1@gmail.com>
2020-02-20 10:18:06 +0545

    Binary Seach Implemented

--- Binary_Search.cpp
+++ Binary_Search.cpp
@@ -41,11 +41,43 @@ void BinarySearch::display(){
 	}
 }
 
+bool BinarySearch::Binary_Search(int data){
+	int min=0;
+	int max=MAX_SIZE-1;
+	while (max>min){
+		int mid=(min+max)/2;
+		if(element[mid]==data){
+			return true;
+		}
+		else if(element[mid]<data){
+			max=mid-1;
+		}
+		else if(element[mid]>data){
+			min=mid+1;
+		}
+	}
+	return false;
+}
+
 int main(){
 	BinarySearch b;
 	b.insert_data(3);
 	b.insert_data(5);
 	b.insert_data(2);
+	b.insert_data(1);
+	b.insert_data(7);
+	b.insert_data(9);
+	b.insert_data(11);
+	b.insert_data(14);
+	b.insert_data(13);
+	b.insert_data(10);
 	b.display();
+	
+	if(b.Binary_Search(5)){
+		std::cout<<"Found"<<std::endl;
+	}
+	else{
+		std::cout<<"Not Found"<<std::endl;
+	}
 
 }
